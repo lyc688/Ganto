9da486821feff32897ac90e2c399829dc1f7e54d
Cevno <62213254+cevno-cn@users.noreply.github.com>
2020-09-12 22:35:21 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,16 @@ $(function(){
 	$("footer span").click(function(){
 		$("body,html").animate({scrollTop:0});
 	})
+	$(".cons .left_").click(function(){
+		$(".cons .left_").hide();
+		$(".cons .right_").show();
+		$(".con").show();
+		$(".cons").css({"left":"50%"});
+	})
+	$(".cons .right_").click(function(){
+		$(".cons .right_").hide();
+		$(".cons .left_").show();
+		$(".con").hide();
+		$(".cons").css({"left":"100%"});
+	})
 })
