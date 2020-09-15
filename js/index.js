d76a5db9a0216ab60912b2cde72e5aede824418b
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-09-15 09:10:23 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,12 +3,14 @@ $(function(){
 		$("body,html").stop().animate({scrollTop:0},300);
 	})
 	$(".cons .left_").click(function(){
+		$("body").css({"overflow":"hidden"});
 		$(".cons .left_").hide();
 		$(".cons .right_").show();
 		$(".con").show();
 		$(".cons").css({"left":"30%","box-shadow":"-4px 0px 4px rgba(0,0,0,0.5)"});
 	})
 	$(".con,.cons .right_").click(function(){
+		$("body").css({"overflow":"auto"});
 		$(".cons .right_").hide();
 		$(".cons .left_").show();
 		$(".con").hide();
