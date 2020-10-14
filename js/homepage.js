c9b00c706935ac8e3216af3cf8685065ef1279c9
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-10-14 15:46:17 +0800

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,1 +1,8 @@
 console.log("%c Ganto的个人主页 https://ganto.cn","background: #0e0e0e;color: white;padding: 10px;border-radius: 4px;");
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?02903c6e6f5f09461f68d07b7887f5ea";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
