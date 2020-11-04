6e1a4dfdca3b62ad3b567043a7f8a3a3ebe3846b
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-11-04 12:04:32 +0800

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,20 +1,20 @@
 window.onload = function(){
-console.log("%c   ____  \n" +
-	"  / ___\\ \n"+
-	" / /   __  \n"+
-	"/ /____\\ \\  \n"+
-	"\\________/ https://ganto.cn","color: #3742FA;"
-);
+	console.log("%c   ____  \n" +
+		"  / ___\\ \n"+
+		" / /   __  \n"+
+		"/ /____\\ \\  \n"+
+		"\\________/ https://ganto.cn","color: #3742FA;"
+	);
 
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?02903c6e6f5f09461f68d07b7887f5ea";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
+	var _hmt = _hmt || [];
+	(function() {
+		var hm = document.createElement("script");
+		hm.src = "https://hm.baidu.com/hm.js?02903c6e6f5f09461f68d07b7887f5ea";
+		var s = document.getElementsByTagName("script")[0]; 
+		s.parentNode.insertBefore(hm, s);
+	})();
 
-document.querySelector(".demo span").onclick=function(){
-window.open("https://live.bilibili.com/6");
-}
+// 	document.querySelector(".demo span").onclick=function(){
+// 		window.open("https://live.bilibili.com/6");
+// 	}
 }
