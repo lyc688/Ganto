1f28d8344f096de9a246b9d39003110c5e314189
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-11-14 14:57:59 +0800

    Create index.js

---
+++ qq/js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+	$("footer ul li").click(function(){
+		$(this).addClass("active").siblings().removeClass("active");
+		$(this).removeClass("unactive").siblings().addClass("unactive");
+	})
+	$(".logo").click(function(){
+		var m_r_w = $(".m_r").width();
+		$(".m_r").animate({left:m_r_w},500);
+		$(".m_l").animate({left:0},500);
+		$("header").css("position","absolute");
+		$("footer").css("position","absolute");
+	});
+	$(".m_l span").click(function(){
+		var m_l_w = $(".m_l").width();
+		$(".m_r").animate({left:0},500);
+		$(".m_l").animate({left:-(m_l_w)},500);
+		setTimeout(function(){
+			$("header").css("position","fixed");
+			$("footer").css("position","fixed");
+		},500);
+	})
+})
