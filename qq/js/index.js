879797dec0e2dc9a429eb1d303db6faf34fa182c
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-11-16 17:12:00 +0800

    Update index.js

--- qq/js/index.js
+++ qq/js/index.js
@@ -20,3 +20,11 @@ $(function(){
 		},500);
 	})
 })
+!document.mozFullScreenElement && !document.webkitFullscreenElement) {// current working methods
+if (document.documentElement.requestFullscreen) {
+	document.documentElement.requestFullscreen();
+} else if (document.documentElement.mozRequestFullScreen) {
+	document.documentElement.mozRequestFullScreen();
+} else if (document.documentElement.webkitRequestFullscreen) {
+	document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+}
