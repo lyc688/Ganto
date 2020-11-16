fb5bcf306982c3254b0b4523549244ad7c5f4fa8
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-11-16 17:13:34 +0800

    Update index.js

--- qq/js/index.js
+++ qq/js/index.js
@@ -20,10 +20,21 @@ $(function(){
 		},500);
 	})
 })
-if (document.documentElement.requestFullscreen) {
-	document.documentElement.requestFullscreen();
-} else if (document.documentElement.mozRequestFullScreen) {
-	document.documentElement.mozRequestFullScreen();
-} else if (document.documentElement.webkitRequestFullscreen) {
-	document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+if (!document.fullscreenElement && // alternative standard method
+	!document.mozFullScreenElement && !document.webkitFullscreenElement) {// current working methods
+	if (document.documentElement.requestFullscreen) {
+		document.documentElement.requestFullscreen();
+	} else if (document.documentElement.mozRequestFullScreen) {
+		document.documentElement.mozRequestFullScreen();
+	} else if (document.documentElement.webkitRequestFullscreen) {
+		document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+	}
+} else {
+	if (document.cancelFullScreen) {
+		document.cancelFullScreen();
+	} else if (document.mozCancelFullScreen) {
+		document.mozCancelFullScreen();
+	} else if (document.webkitCancelFullScreen) {
+		document.webkitCancelFullScreen();
+	}
 }
