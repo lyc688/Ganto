825af1eff96dedb7ee28d005aa6eab580f0b8ce0
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-11-29 21:28:42 +0800

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -4,58 +4,30 @@ function judge(){
 	if(localStorage.getItem("theme") === "dark"){
 		dark.setAttribute("media","all");
 		light.setAttribute("media","not all");
-		console.log(localStorage.getItem("num"),typeof localStorage.getItem("num"));
-// 		if(localStorage.getItem("num") === "0"){
-// 			document.getElementsByTagName("a")[0].style.color = "white";
-// 		}else if(localStorage.getItem("num") === "1"){
-// 			document.getElementsByTagName("a")[1].style.color = "white";
-// 		}else if(localStorage.getItem("num") === "2"){
-// 			document.getElementsByTagName("a")[2].style.color = "white"; 
-// 		}else if(localStorage.getItem("num") === "3"){
-// 			document.getElementsByTagName("a")[3].style.color = "white"; 
-// 		}else{
-// 			localStorage.setItem("num","0");
-// 		}
+
 	}else if(localStorage.getItem("theme") === "light"){
 		dark.setAttribute("media","not all");
 		light.setAttribute("media","all");
-// 		if(localStorage.getItem("num") === "0"){
-// 			document.getElementsByTagName("a")[0].style.color = "black";
-// 		}else if(localStorage.getItem("num") === "1"){
-// 			document.getElementsByTagName("a")[1].style.color = "black";
-// 		}else if(localStorage.getItem("num") === "2"){
-// 			document.getElementsByTagName("a")[2].style.color = "black"; 
-// 		}else if(localStorage.getItem("num") === "3"){
-// 			document.getElementsByTagName("a")[3].style.color = "black"; 
-// 		}else{
-// 			localStorage.setItem("num","0");
-// 		}
 	}else{
 		localStorage.setItem("theme","light");
 	}
-	
-	
-	
 }
 (() => {
 	judge();
 })();
 window.onload = function(){
-	var a = document.getElementsByTagName('a'); // 获取所有li元素
-	for(var j=0; j<a.length; j++){
-		a[j].index = j; // 为第i个li元素添加一个index属性，赋值为i
-		a[j].addEventListener('click',function() {
-			localStorage.setItem("num",this.index);
-			// 为第i个li元素添加一个click事件，当元素被点击时，则会在控制台输出索引
-		})
-	}
 	if(localStorage.getItem("theme") === "dark"){
-		console.log(parseInt(localStorage.getItem("num")),localStorage.getItem("num"));
 		document.getElementsByTagName("a")[parseInt(localStorage.getItem("num"))].style.color = "white";
 	}else{
-		console.log(parseInt(localStorage.getItem("num")),localStorage.getItem("num"));
 		document.getElementsByTagName("a")[parseInt(localStorage.getItem("num"))].style.color = "black";
-
+	}
+	var a = document.getElementsByTagName('a'); // 获取所有a元素
+	for(var j=0; j<a.length; j++){
+		a[j].index = j; // 为第j个a元素添加一个index属性，赋值为j
+		a[j].addEventListener('click',function() {
+			localStorage.setItem("num",this.index);
+			// 为第j个a元素添加一个click事件，当元素被点击时，则会设置localStorage储存起来
+		})
 	}
 	document.querySelector(".toggle").onclick = function(){
 		if(localStorage.getItem("theme") === "dark"){
