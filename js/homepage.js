03aa9e7733316019326c751e65ff9e308e4ce6f5
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-11-30 23:44:16 +0800

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -34,20 +34,6 @@ window.onload = function(){
 		localStorage.removeItem("state");
 		judge();
 	}
-	console.log("%c   ____  \n" +
-		"  / ___\\ \n"+
-		" / /   __  \n"+
-		"/ /____\\ \\  \n"+
-		"\\________/ https://ganto.cn","color: #3742FA;"
-	);
-
-	var _hmt = _hmt || [];
-	(function() {
-		var hm = document.createElement("script");
-		hm.src = "https://hm.baidu.com/hm.js?02903c6e6f5f09461f68d07b7887f5ea";
-		var s = document.getElementsByTagName("script")[0]; 
-		s.parentNode.insertBefore(hm, s);
-	})();
 // 	document.querySelector(".demo span").onclick=function(){
 // 		window.open("https://live.bilibili.com/6");
 // 	}
