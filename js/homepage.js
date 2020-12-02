600ae42592b8097b14921db3b381fb6f7804adbf
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-12-02 18:57:33 +0800

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -23,28 +23,25 @@ window.onload = function(){
 	
 	// 通知
 	var title = "小站已经适配深色模式";
-	var content = "测试1";
+	var content = "点击辣条切换吧点击辣条切换吧点击辣条切换吧点击辣条切换吧点1击辣条切换吧点击辣条切换吧点击辣条切换吧点击辣条切换吧点击辣条切换吧点击辣条切换吧点击辣条切换吧点击辣条切换吧";
 	//localStorage.setItem("flag","true"); // 如果需要强制一直显示通知，请取消此条代码注释
 	if(localStorage.getItem("flag") === "true" && localStorage.getItem("content") === content){
 		document.querySelector(".title").innerHTML = title;
 		document.querySelector(".content").innerHTML = content;
 		document.querySelector(".notice").style.display = "block";
-		console.log("0");
 	}else if(localStorage.getItem("flag") === "true" && localStorage.getItem("content") !== content){
 		document.querySelector(".title").innerHTML = title;
 		document.querySelector(".content").innerHTML = content;
 		document.querySelector(".notice").style.display = "block";
-		console.log("1");
-	}else if(localStorage.getItem("flag") === "false" && localStorage.getItem("content") === content){
-		console.log("2");
+	}else if(localStorage.getItem("flag") !== "true" && localStorage.getItem("content") === content){
+		localStorage.setItem("flag","false");
 		return;
 	}else if(localStorage.getItem("flag") !== "true" && localStorage.getItem("content") !== content){
 		document.querySelector(".title").innerHTML = title;
 		document.querySelector(".content").innerHTML = content;
 		document.querySelector(".notice").style.display = "block";
 		localStorage.setItem("content",content);
-		localStorage.setItem("flag","ture");
-		console.log("3");
+		localStorage.setItem("flag","true");
 	}
 	document.querySelector(".off").onclick = function(){
 		document.querySelector(".notice").style.display = "none";
