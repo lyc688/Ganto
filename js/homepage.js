2e16643729148adf2a69535d29b07b7434ec4005
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2020-12-05 22:05:14 +0800

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -35,9 +35,9 @@ window.onload = function(){
 	// 通知
 // 	var title = "小站已经适配深色模式";
 	var title = "biubiu送拳头账号";
-// 	var content = "单击辣条可以切换深色/浅色模式，也可自动响应系统的深色/浅色模式。但是依然遵循人为喜好优先的原则。";
-	var content = "新用户可上车，小队满3人以上全员的LOL手游台服拳头账号，活动12月8日截止。速来上车<a href='http://test001.zkouyu003.com/activity/lol-tw-share?toolbar_mode=1&si=8NyQBwlcH%2FqXKwfK9s10qdVV6RxnLV1a%2BxyCcg2AR5JwBpad0MUdLUL24LNt7tWwyQ%2BUYSO%2BSqFNwtvxpnpGBObsw6uepXbt1%2F8heh%2FqxlrhQt7aMCvOQ1LZCJ5WkE9ho0FLlqQ1r%2F3zfC6WG6UN5w%3D%3D&cfrom=banner&_platform=android&_ver=3.17.2&_ch=BIU_17&_sharepage=activity_lol_tw_share&_sharemethod=fzlj
-'>点击链接上车</a>";
+	var content = "单击辣条可以切换深色/浅色模式，也可自动响应系统的深色/浅色模式。但是依然遵循人为喜好优先的原则。";
+// 	var content = "新用户可上车，小队满3人以上全员的LOL手游台服拳头账号，活动12月8日截止。速来上车<a href='http://test001.zkouyu003.com/activity/lol-tw-share?toolbar_mode=1&si=8NyQBwlcH%2FqXKwfK9s10qdVV6RxnLV1a%2BxyCcg2AR5JwBpad0MUdLUL24LNt7tWwyQ%2BUYSO%2BSqFNwtvxpnpGBObsw6uepXbt1%2F8heh%2FqxlrhQt7aMCvOQ1LZCJ5WkE9ho0FLlqQ1r%2F3zfC6WG6UN5w%3D%3D&cfrom=banner&_platform=android&_ver=3.17.2&_ch=BIU_17&_sharepage=activity_lol_tw_share&_sharemethod=fzlj
+// '>点击链接上车</a>";
 	//localStorage.setItem("flag","true"); // 如果需要强制一直显示通知，请取消此条代码注释
 	if(localStorage.getItem("flag") === "true" && localStorage.getItem("content") === content){
 		document.querySelector(".title").innerHTML = title;
