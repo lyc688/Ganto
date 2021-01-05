037005098023704f10dbd16a689b3fd5c8116678
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2021-01-05 15:02:54 +0800

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,13 +1,13 @@
 function a(e){
 	console.log(e.pageX, e.pageY);
-	const main = document.querySelector("body");
+	const body = document.querySelector("body");
 	const span = document.createElement("span");
 	const i = document.createElement("i");
 	span.classList.add("click");
 	span.style.top = e.pageY + "px";
 	span.style.left = e.pageX + "px";
 	span.append(i);
-	main.append(span);
+	body.append(span);
 	setTimeout(() => {
 		span.remove();
 	}, 500);
