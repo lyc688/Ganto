624669e800fa7258c47c244a87bc24010a05344d
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2021-01-05 14:42:23 +0800

    Create click.js

---
+++ js/click.js
@@ -0,0 +1,16 @@
+function a(e){
+	console.log(e.pageX, e.pageY);
+	const main = document.querySelector("body");
+	const span = document.createElement("span");
+	const i = document.createElement("i");
+	span.style.top = e.pageY + "px";
+	span.style.left = e.pageX + "px";
+	span.append(i);
+	main.append(span);
+	setTimeout(() => {
+		span.remove();
+	}, 500);
+}
+document.querySelector("html").onclick = function(e){
+	a(e);
+}
