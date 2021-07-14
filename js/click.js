a50561015ef991728e347fa7c054541f7af1880f
ganto-cn <71207296+ganto-cn@users.noreply.github.com>
2021-07-14 14:50:22 +0800

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,17 +1,16 @@
 function a(e){
-// 	console.log(e.pageX, e.pageY);
+	// console.log(e.pageX, e.pageY);
 	const body = document.querySelector("body");
 	const span = document.createElement("span");
-// 	const i = document.createElement("i");
 	span.classList.add("click");
 	span.style.top = e.pageY + "px";
 	span.style.left = e.pageX + "px";
-// 	span.append(i);
 	body.append(span);
 	setTimeout(() => {
 		span.remove();
-	}, 500);
+	}, 750);
 }
 document.querySelector("html").onclick = function(e){
 	a(e);
+	// console.log(e);
 }
